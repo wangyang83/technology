35fd30abe54e92ee17357f01f98b6c7f092e4f05
wangyang <2258525102@>
2020-04-09 10:46:27 +0800

    上传springboot框架

---
+++ springBoot运用redis技术/src/main/java/com/redis/SpringBootRedis.java
@@ -0,0 +1,16 @@
+package com.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王阳
+ * @date 2020/4/9 10:28
+ */
+@SpringBootApplication
+public class SpringBootRedis {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedis.class,args);
+    }
+}
